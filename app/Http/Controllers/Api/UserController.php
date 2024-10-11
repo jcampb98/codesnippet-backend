184a39abbf1f45ad7a86ac0a2549995091c240b3
jcampb98 <caboose768@gmail.com>
2024-10-11 15:09:31 +0100

    fixed a small type in the UserController

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 {
 
     public function __construct() {
-        $this -> middleware("auth:api", ['except' => ['createUser', 'loginUser']]);
+        $this -> middleware("auth:api", ['except' => ['register', 'login']]);
     }
 
     public function register(Request $request) {
